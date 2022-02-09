c840a327e7359876e99401ae53fa3202d2cfc2ab
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2022-02-09 18:47:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const qrcode = require('qrcode-terminal');
+const thumb = './assets/thumb.png
 
 const { Client } = require('whatsapp-web.js');
 const client = new Client();
@@ -15,4 +16,10 @@ client.initialize();
 
 client.on('message', message => {
 	console.log(message.body);
+});
+
+client.on('message', message => {
+if (msg.body === '!buttons') {
+        let button = new Buttons('Button MessageMediafromFilePath(thumb)',[{body:'bt1'},{body:'bt2'},{body:'bt3'}],'title','footer');
+        client.sendMessage(msg.from, button); }
 });
